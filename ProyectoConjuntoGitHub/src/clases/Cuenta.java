08c969decce7debdf1beae7e696e55cd31afb85b
danielamrttz <danielamartinezfernandez10@gmail.com>
2024-04-30 17:15:20 +0200

    Clase Cuenta

--- ProyectoConjuntoGitHub/src/clases/Cuenta.java
+++ ProyectoConjuntoGitHub/src/clases/Cuenta.java
@@ -1,5 +1,31 @@
 package clases;
 
+
 public class Cuenta {
+	
+	
+	private String iban;
+	private Cliente cliente;
+	private double cantidad;
+	private String estado;
+	
+	/**
+	 *  Constructor de la clase cuenta
+	 *  
+	 * @param iban			Pide iban
+	 * @param cliente		Pide cliente
+	 * @param cantidad		Pide cantidad
+	 * @param estado		Estado por defecto "Activa"
+	 */
+	
+	public Cuenta(String iban, Cliente cliente, double cantidad, String estado) {
+		
+		this.iban = iban;
+		this.cliente = cliente;
+		this.cantidad = cantidad;
+		this.estado = "Activa";
+	}
+	
+	
 
 }
