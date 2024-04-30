ac9d30fa62bee10b1522a310eca538bb274de34d
danielamrttz <danielamartinezfernandez10@gmail.com>
2024-04-30 17:17:06 +0200

    Clase cuenta creada y corregida

--- ProyectoConjuntoGitHub/src/clases/Cuenta.java
+++ ProyectoConjuntoGitHub/src/clases/Cuenta.java
@@ -25,6 +25,40 @@ public class Cuenta {
 		this.cantidad = cantidad;
 		this.estado = "Activa";
 	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
 	
 	
 
