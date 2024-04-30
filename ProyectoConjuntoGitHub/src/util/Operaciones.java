b58b6bfd2d82c545000f60c275c00e24cae79231
fran-quero <fjavier.fj41@gmail.com>
2024-04-30 17:25:29 +0200

    Clase operaciones completada

--- ProyectoConjuntoGitHub/src/util/Operaciones.java
+++ ProyectoConjuntoGitHub/src/util/Operaciones.java
@@ -1,5 +1,53 @@
 package util;
 
+import clases.Cuenta;
+
 public class Operaciones {
+	
+	/**
+	 * Transfiere si tiene suficiente cantidad la cuenta origen la cantidad indicada a otra
+	 * @param cuentaOrigen
+	 * @param cuentaDestino
+	 * @param cantidad
+	 */
+	public static void transferencia(Cuenta cuentaOrigen, Cuenta cuentaDestino, Double cantidad) {
+		
+		if(cuentaOrigen.getCantidad()>=cantidad) {
+			cuentaOrigen.setCantidad(cuentaOrigen.getCantidad()-cantidad);
+			cuentaDestino.setCantidad(cuentaDestino.getCantidad()+cantidad);
+			System.out.println("Transferencia realizada");
+		}
+		else {
+			System.out.println("No tiene cantidad suficiente");
+		}
+		
+	}
+	
+	/**
+	 * Se presta una cantidad indicada a la cuenta indicada
+	 * 
+	 * @param cuentaDestino
+	 * @param cantidad
+	 */
+	public static void prestamo(Cuenta cuentaDestino, Double cantidad) {
+			
+			
+		cuentaDestino.setCantidad(cuentaDestino.getCantidad()+cantidad);
+		System.out.println("Cantidad prestada");
+		
+	}
+	
+	/**
+	 * Cambia el estado de una cuenta a cancelado
+	 * 
+	 * @param cuentaDestino
+	 */
+	public static void cancelarCuenta(Cuenta cuentaDestino) {
+			
+		cuentaDestino.setEstado("Cancelada");
+		
+	}
+	
+	
 
 }
