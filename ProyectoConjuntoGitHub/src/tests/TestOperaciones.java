14835319b7a73c2dd3048e37834c57533ad13fef
fran-quero <fjavier.fj41@gmail.com>
2024-04-30 17:01:56 +0200

    Creado las carpetas con los ficheros que se pide

---
+++ ProyectoConjuntoGitHub/src/tests/TestOperaciones.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestOperaciones {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
