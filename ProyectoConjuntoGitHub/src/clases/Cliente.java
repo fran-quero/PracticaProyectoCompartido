e3008e1e3ebb18c75c0d13f69b552d312b1c7908
unknown <Robert5022>
2024-04-30 17:11:53 +0200

    Clase cliente creada

--- ProyectoConjuntoGitHub/src/clases/Cliente.java
+++ ProyectoConjuntoGitHub/src/clases/Cliente.java
@@ -1,5 +1,22 @@
 package clases;
 
-public class Cliente {
 
+public class Cliente {
+	
+	private String nombre;
+	private String apellidos;
+	private int edad;
+
+	
+	/**
+	 * Costructor por defecto con los parametros 
+	 * @param nombre nombre del cliente 
+	 * @param apellidos apellidos del cliente
+	 * @param edad edad del cliente
+	 */
+	public Cliente(String nombre, String apellidos, int edad) {
+	    this.nombre = nombre;
+	    this.apellidos = apellidos;
+	    this.edad = edad;
+	}
 }
