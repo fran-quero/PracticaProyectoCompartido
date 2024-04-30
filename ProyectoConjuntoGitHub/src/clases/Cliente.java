cee1202b9c4a174a70eeab6c188b8a27d062cd24
unknown <Robert5022>
2024-04-30 17:18:16 +0200

    Los getter y setter

--- ProyectoConjuntoGitHub/src/clases/Cliente.java
+++ ProyectoConjuntoGitHub/src/clases/Cliente.java
@@ -19,4 +19,35 @@ public class Cliente {
 	    this.apellidos = apellidos;
 	    this.edad = edad;
 	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 }
